43e35edfeac819af081de808a65896d91f4ecb0d
tatsuro1997 <minagawa@dptr.jp>
2022-05-27 08:01:16 +0900

    Rendering Dummy Post Data

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -7,8 +7,8 @@ function PostsGrid(props) {
 
   return (
     <ul className={classes.grid}>
-      {posts.mao((post) => (
-        <PostItem />
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post} />
       ))}
     </ul>
   );
