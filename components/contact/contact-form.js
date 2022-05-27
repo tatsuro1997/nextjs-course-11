d7c35b965202683557b64c13f8f4673f26a1bd15
tatsuro1997 <minagawa@dptr.jp>
2022-05-28 05:25:34 +0900

    Update contact-form.js

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -72,7 +72,7 @@ function ContactForm() {
     notification = {
       status: "success",
       title: "Success!",
-      message: "Message sent successfully!",
+      message: "Message sent successfully!!!!!!!!!",
     };
   }
 
