607a51092cf7c2c7932239b619b65f70b8f2ce99
tatsuro1997 <minagawa@dptr.jp>
2022-05-27 09:03:02 +0900

    Working On The "Post Detail" Page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,25 @@
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
+const DUMMY_POST = {
+  slug: "getting-started-with-nextjs",
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  excerpt:
+    "NextJS is the React framework for production - it makes building fullstack Re...",
+  date: "2022-02-10",
+  content: "# This is a first post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
